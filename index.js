e229316f885b1aecbfb7c382a632910c692df691
giovanyosorio <giovany.torres.799@gmail.com>
2024-01-02 18:11:45 -0500

    graphql persons

---
+++ index.js
@@ -0,0 +1,25 @@
+const persons=[
+    {
+        name: 'John',
+        age: 30,
+        phone:"1234567890",
+        city:"Bogota",
+        id:"123asfadsfasad-asdfasdfasdf-asdfasdfasdf"
+    },
+    {
+        name:"Raj",
+        age: 20,
+        phone:"1234567890",
+        city:"Barcelona",
+        id:"123asfa322343d-asdfasdfasdf-asdfasdfasdf"
+    },
+    {
+        name:"Itzi",
+        age: 25,
+        phone:"1456324324",
+        city:"Madrid",
+        id:"123asfadsfasad-asdfa1231235f-asdfasdfasdf"
+    }
+]
+
+const typeDefs = ``
